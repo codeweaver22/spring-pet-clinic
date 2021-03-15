7bdc37ff4deb1e7881c6d1e4209292901a4a4959
Sindhuja <sindhuja22@protonmail.com>
2021-03-15 12:55:00 +0530

    Initial Project Add

---
+++ src/main/java/com/codeweaver/springpetclinic/SpringPetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeweaver.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeweaver/springpetclinic/SpringPetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeweaver.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
