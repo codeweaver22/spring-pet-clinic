95cc5d07e6b56239eea2b9cf4bcf68d7e199be27
Sindhuja <sindhuja22@protonmail.com>
2021-03-17 17:38:46 +0530

    adding POJOs. closes #8.

--- src/main/java/com/codeweaver/springpetclinic/SpringPetClinicProjectApplication.java
+++ src/main/java/com/codeweaver/springpetclinic/SpringPetClinicProjectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPetClinicProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPetClinicProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicProjectApplication.class, args);
+
+    }
 
 }
