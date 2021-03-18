fb4c1e677df4272e76ebf513231f0c91ebfbefde
Sindhuja <sindhuja22@protonmail.com>
2021-03-18 14:15:21 +0530

    Added BaseEntity. closes #10

--- pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/models/Pet.java
@@ -2,7 +2,7 @@ package com.codeweaver.springpetclinic.models;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/models/PetType.java
+++ pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/models/PetType.java
@@ -1,6 +1,6 @@
 package com.codeweaver.springpetclinic.models;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
