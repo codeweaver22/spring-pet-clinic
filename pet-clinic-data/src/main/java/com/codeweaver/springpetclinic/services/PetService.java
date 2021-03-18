9599fb13f9d5cb13789c4ad4b9a55e606217919e
Sindhuja <sindhuja22@protonmail.com>
2021-03-18 19:35:36 +0530

    Refactor interfaces to common interface. closes #11

--- pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package com.codeweaver.springpetclinic.services;
 
 import com.codeweaver.springpetclinic.models.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/codeweaver/springpetclinic/services/VetService.java
@@ -2,12 +2,5 @@ package com.codeweaver.springpetclinic.services;
 
 import com.codeweaver.springpetclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
