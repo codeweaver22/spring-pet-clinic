552c82cef36bd1da0d4268026c1b5b13ffbccdc5
Sindhuja <sindhuja22@protonmail.com>
2021-04-08 17:27:50 +0530

    Display list of vets. Closes #15

--- pet-clinic-web/src/main/java/com/codeweaver/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/codeweaver/springpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package com.codeweaver.springpetclinic.controllers;
 
+import com.codeweaver.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"",     "/", "/index", "/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
